f92c6f7ca1be21651bd2e3de754af5c8725f30bc
Sean Mapanao <sean.mapanao@gmail.com>
2022-09-01 23:33:04 -0400

    Finished form and showing a list of dogs

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -6,23 +6,35 @@ namespace netpractice1.Controllers;
 
 public class HelloWorldController : Controller{
 
+    //since we're not using a database, we will just temporarily use a list
+    private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+    //view index page
     public IActionResult Index()
     {
-        DogViewModel doggo = new DogViewModel(){
-            Name = "Gucci", Age = 2
-        };
-
-        return View(doggo);
+   
+        return View(dogs);
     }
 
+    //this method allows us to view the form
     public IActionResult Create(){
         //before we load the page, we want to make a new dogVm object where the data will be binded to it from the form
+        //returns to the view in the HelloWorld folder, that conatins a Create.cshtml
         var dogVm = new DogViewModel();
         return View(dogVm);
     }
 
+    //this method allows us to bind the information 
     public IActionResult CreateDog(DogViewModel dogViewModel){
-        return View("Index");
+        
+        //add the current args to list
+        dogs.Add(dogViewModel);
+
+        //this returns to the index view method 
+        return RedirectToAction(nameof(Index));
+        
+        //below returns to the chtml
+        //return View("Index");
     }
 
 }
